d0da3ce633683622fedaf146455ea6b8c45dbf93
Opeyemi Ajayi <elonmuskusa61@gmail.com>
2026-09-18 08:58:37 -0700

    hmm

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Temples JavaScript loaded successfully.");
-});
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
